2193ff19cb5c7359c1b025f0d3d961f53ebaf9c8
Bergur Hallgrimsson <bergur@bergur.is>
2018-01-08 09:15:28 +0000

    Digital numbers in countdown

--- src/components/countdown.vue
+++ src/components/countdown.vue
@@ -1,33 +1,43 @@
 re<template> 
       <div>   
-        <div>
-            <img src="figure-0.png" class="figure" />
-            <img src="images/figure-0.png" class="figure" />
-            <img src="/images/figure-colon.png" />            
-        </div>
+        <div style="text-align:center">                
+          <img :src="numbers[0]" />
+          <img :src="numbers[1]" />
+          <img src="../images/figure-colon.png" />
+          <img :src="numbers[2]" />
+          <img :src="numbers[3]" />
+        </div>        
         <v-ons-button modifier="large" @click="() => on ? stop() : start()" style="margin: 6px 0">{{ on ? 'Stopp' : 'Byrja' }}</v-ons-button>      
     </div>   
 </template>
 
-<script>  
+<script>
+  import figurec from '../images/figure-0.png';
+  import figure0 from '../images/figure-0.png';
+  import figure1 from '../images/figure-1.png';
+  import figure2 from '../images/figure-2.png';
+  import figure3 from '../images/figure-3.png';
+  import figure4 from '../images/figure-4.png';
+  import figure5 from '../images/figure-5.png';
+  import figure6 from '../images/figure-6.png';
+  import figure7 from '../images/figure-7.png';
+  import figure8 from '../images/figure-8.png';
+  import figure9 from '../images/figure-9.png';
+
   export default {
     props: ['startAt'],
     data() {
       return { 
         on: false,
         intervalId: null,
-        value: this.startAt* 1000
+        value: this.startAt* 1000,
+        numbers: [figure0,figure1,figure2,figure3,figure4,figure5,figure6,figure7,figure8,figure9]
       };
     }, 
     computed: {
-      second() {
-        const s = Math.floor(this.value/1000);
-        return s >= 10 ? s : '0'+s;
-      },
-      millisecond() {
-        const ms = Math.floor(this.value%10000)/100;
-        return ms >= 10 ? ms : '0'+ms;
-      }    
+      time() {
+        
+      }      
     },
     methods: {
       start() {
